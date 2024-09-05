80cc1d20cf16a01786d857655f1bdf12d52e48c8
ZacharyMcMillan-weber <157736646+ZacharyMcMillan-weber@users.noreply.github.com>
2024-09-05 06:46:07 +0000

    finally

--- Notes/cppConditionals.cpp
+++ Notes/cppConditionals.cpp
@@ -2,6 +2,7 @@
 # include <string>
 using namespace std;
 
+/*
 int main() {
     short day;
     string name;
@@ -15,6 +16,26 @@ int main() {
     else if (day == 6){name = "Friday"; }
     else if (day == 7){name = "Saturday";   }
     else {name = "Invalid Name";    }
+    cout << "Day of the week is " << name << endl;
+    return 0;
+}
+*/
+// This is the same as above only this time we are using switch cases
+
+int main() {
+    short day; string name = "";
+    cout << "Enter a day number(between 1 and 7): " << endl; cin >> day;
+    switch (day)
+    {
+    case 1: name = "Sunday"; break;
+    case 2: name = "Monday"; break;
+    case 3: name = "Tuesday"; break;
+    case 4: name = "Wednesday"; break;
+    case 5: name = "Thurday"; break;
+    case 6: name = "Friday"; break;
+    case 7: name = "Saturday"; break;
+    default: name = "Invalid input";
+    }
     cout << "Day of the week is " << name << endl;
     return 0;
 }
