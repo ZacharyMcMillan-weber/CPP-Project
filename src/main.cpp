da422c8aaa26145a22dada24e2b80e43ec7c83aa
Zach McMillan <zacharymcmillan@mail.weber.edu>
2024-09-05 11:50:52 -0600

    week 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,8 @@
 #include <iostream>
-using namespace std;
 
 int main(){
     // TODO
-    cout << "What Score did you get for Notebook1?";
-    double score;
-    cin >> score;
-
-    char letterGrade;
-
-    if(score >= 9){
-        letterGrade = 'A';
-    } else if(score >= 8){
-        letterGrade = 'B';
-    } else if(score >= 7){
-        letterGrade = 'C';
-    } else {
-        letterGrade = 'D';
-
-    }
-
-    if(-1){
-        cout << "Must be true to get here\n";
-    }
-
-    cout << "A score of " << score << " means " << letterGrade << endl;
+    std::cout << "hello Zachary McMillan" << std::endl;
+    
     return 0;
 }
