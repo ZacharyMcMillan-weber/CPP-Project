14824864bacee80c0aac32dc5b84c740874760ea
Zach McMillan <zacharymcmillan@mail.weber.edu>
2024-10-17 18:21:37 +0000

    week08 done

--- src/week08.cpp
+++ src/week08.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 class Account {
     private:
@@ -12,10 +13,27 @@ class Account {
     public:
     // Constructor -- Same as __init__ in python
         Account(int id):id(id), balance(0.0), type("Checking"), rate(0.0001){} 
-        Account(int id, double b, std::string t, double rate):id(id), balance(b), type(t), rate(rate){
-            
-};
+        Account(int id, double b, std::string t, double rate):id(id), balance(b), type(t), rate(rate){}
 
+    int getID() const { return id;} // Getter / Accessor 
+    void setID(int id) {this->id = id;} // Setter, Mutator only need to use 'this->' when setting new name to the same as the old name 
+
+    int getBalance() const { return balance; }
+    void setBalance(double b) {balance = b;}
+
+    const std::string&  getType() const{return type;}
+    void setType(const std::string& type){this->type = type;}
+  
+    double getRate() const{return rate;}
+    void setRate(double r) {rate = r;}
+    
+    void show(){
+        std:: cout << std::setw(10) << "ID: " << id << std::endl
+        << std::setw(10) << "BALANCE: " << balance << std::endl
+        << std::setw(10) << "ACCOUNT TYPE: " << type << std::endl
+        << std::setw(10) << "RATE: " << rate << std::endl;
+        
+    }
 };
 
 int main() {
@@ -25,8 +43,12 @@ int main() {
     Account* c = new Account(345, 100.0, "Checking", 0.005);
     Account* d = new Account(456);
 
-    
+    a.show();
+    b.show();
+    c->show();
+    d->show();
+
     delete c, d;
-    
+
     return 0;
 }
